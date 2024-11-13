bba23e06f7d9767d812da051a0bdabed286ce326
Ohyoung Song <songohyoung@gmail.com>
2024-11-14 00:42:47 +0900

    localStream from camera added

---
+++ src/useLocalCameraStream.tsx
@@ -0,0 +1,18 @@
+
+import { useEffect, useState } from 'react';
+ 
+export function useLocalCameraStream() {
+  const [localStream, setLocalStream] = useState<MediaStream | null>(null);
+ 
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        setLocalStream(stream);
+      });
+  }, []);
+ 
+  return {
+    localStream,
+  };
+}
